687c8700e1f481d08a83ac85e9d58e8a8f2512e0
surajbca <krsurajkumar7481@gmail.com>
2023-04-17 19:31:05 +0530

    Impleted UI

--- Blobmaker/ButtonColor.js
+++ Blobmaker/ButtonColor.js
@@ -5,134 +5,135 @@ function buttonColor() {
 }
 
 function updateInput(value) {
-    alert("");
   document.getElementById("inputValue") =
     document.getElementById("btn1").value;
-
      document.getElementById("inputValue") =
     document.getElementById("btn2").value;
 }
 
 function btn1(color) {
- alert('color: ' + color)
   const btn1 = document.getElementById("buttonColor");
+  const circle1 = document.getElementById("circle1");
   btn1.style.background = color;
   btn1.style.border = color;
-
+  circle1.style.fill = color;
   document.getElementById("inputValue").value = color;
 }
 
 function btn2(color) {
-  
   const btn2 = document.getElementById("buttonColor");
+  const circle1 = document.getElementById("circle1");
   btn2.style.background = color;
   btn2.style.border = color;
+  circle1.style.fill = color;
   document.getElementById("inputValue").value = color;
 }
 
 function btn3(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn3 = document.getElementById("buttonColor");
   btn3.style.background = color;
   btn3.style.border = color;
-
-   document.getElementById("inputValue").value = color;
+  circle1.style.fill = color;
+  document.getElementById("inputValue").value = color;
 }
 
 function btn4(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn4 = document.getElementById("buttonColor");
   btn4.style.background = color;
   btn4.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn5(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn5 = document.getElementById("buttonColor");
   btn5.style.background = color;
   btn5.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn6(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn6 = document.getElementById("buttonColor");
   btn6.style.background = color;
    btn6.style.border = color;
-
+   circle1.style.fill = color;
     document.getElementById("inputValue").value = color;
 }
 
 function btn7(color) {
- 
+  const circle1 = document.getElementById("circle1");
   const btn7 = document.getElementById("buttonColor");
   btn7.style.background = color;
   btn7.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn8(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn8 = document.getElementById("buttonColor");
   btn8.style.background = color;
   btn8.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn9(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn9 = document.getElementById("buttonColor");
   btn9.style.background = color;
    btn9.style.border = color;
-
+   circle1.style.fill = color;
     document.getElementById("inputValue").value = color;
 }
 
 function btn10(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn10 = document.getElementById("buttonColor");
   btn10.style.background = color;
   btn10.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn11(color) {
-  
+  const circle1 = document.getElementById("circle1");
   const btn11 = document.getElementById("buttonColor");
   btn11.style.background = color;
   btn11.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn12(color) {
- 
+  const circle1 = document.getElementById("circle1");
   const btn12 = document.getElementById("buttonColor");
   btn12.style.background = color;
   btn12.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn13(color) {
- 
+  const circle1 = document.getElementById("circle1");
   const btn13 = document.getElementById("buttonColor");
   btn13.style.background = color;
   btn13.style.border = color;
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
 
 function btn14(color) {
- 
+  const circle1 = document.getElementById("circle1");
   const btn14 = document.getElementById("buttonColor");
   btn14.style.background = color;
   btn14.style.border = color;
-
+  circle1.style.fill = color;
    document.getElementById("inputValue").value = color;
 }
+
--- Blobmaker/Download.js
+++ Blobmaker/Download.js
@@ -1,7 +1,21 @@
-content = document.getElementById("svgImg");
 function downloadImage() {
-  alert("svgImg");
+  let content = document.getElementById("svgImg");
+  alert("content : " + content);
   domtoimage.toBlob(content).then(function (blob) {
-    window.saveAs(blob, "my-node.png");
+    window.saveAs(blob, "my-node.svg");
   });
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var circle = document.querySelector("#circle1");
+setInterval(function () {
+  circle.setAttribute("r", getRandomInt(5, 50));
+}, 1000);
+
+
+#circle1 {
+  transition: r 0.2s;
 }
